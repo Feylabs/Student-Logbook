711a4bb058790df97ddcbf25a74a8971434b968d
henryaugusta <henryaugusta4@gmail.com>
2021-03-28 22:23:34 +0700

    - add insert

---
+++ resources/views/components/message.blade.php
@@ -0,0 +1,36 @@
+@if(session() -> has('success'))
+<div class="alert alert-primary alert-dismissible fade show mx-2 my-2" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        <span class="sr-only">Close</span>
+    </button>
+
+    <script>
+        var notify = $.notify('<strong>Saving</strong> Do not close this page...', { allow_dismiss: false });
+            notify.update({ type: 'success', '<strong>Success</strong> Your page has been saved!' });
+    </script>
+
+    <strong>{{Session::get( 'success' )}}</strong>
+</div>
+
+@elseif(session() -> has('error'))
+
+<div class="alert alert-primary alert-dismissible fade show mx-2 my-2" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        <span class="sr-only">Close</span>
+    </button>
+    <strong>{{Session::get( 'error' )}}</strong>
+</div>
+@endif
+
+@if ($errors->any())
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+        <span class="sr-only">Close</span>
+    </button>
+
+    {!! implode('', $errors->all('<div>:message</div>')) !!}
+</div>
+@endif
