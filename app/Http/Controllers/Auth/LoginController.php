4898de3f7335c32099f08475b15eed168a13ab3b
henryaugusta <henryaugusta4@gmail.com>
2021-04-14 09:59:50 +0700

    Redirect If Auth Bug Fixed
    this bug fixed by creating a new middleware that set to view-route of
    login, then doing Auth::guard check in that

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -28,7 +29,14 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/home';
+    protected function redirectTo()
+    {
+        if (auth()->user()->role == 'admin') {
+            return '/admin';
+        }
+        return '/home';
+    }
 
     /**
      * Create a new controller instance.
@@ -39,6 +47,7 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
 
     public function adminLogin(Request $request)
     {
@@ -62,6 +71,9 @@ class LoginController extends Controller
         }
         return back()->withInput($request->only('nisn', 'remember'));
     }
+
+
+
 
     public function santriLogin(Request $request)
     {
@@ -102,5 +114,29 @@ class LoginController extends Controller
             ]);
         }
         return back()->withInput($request->only('nis', 'remember'));
+    }
+
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $this->guard()->logout();
+        Auth::guard('guru')->logout();
+        Auth::guard('santri')->logout();
+        Auth::guard('admin')->logout();
+
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect('/');
     }
 }
